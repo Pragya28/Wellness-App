bbf5cd9295a3e0afd19964a29aaf627ae9cfd511
Pragya <pragyajha2812@msitprogram.net>
2021-11-24 09:44:05 +0530

    added test for route status

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,3 +12,58 @@ def client():
 def test_start(client):
     resp = client.get("/")
     assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/home")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/profile")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/login")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/logout")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/sign-up")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/bmi")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/calorie")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/sleep")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/water")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/activity")
+    assert resp.status_code == 200
+
+
+def test_start(client):
+    resp = client.get("/learning")
+    assert resp.status_code == 200
